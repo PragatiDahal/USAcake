8acf5f643e0e242aa187e8fca7f7b2652e2b387d
PragatiDahal <pragatidahal2058@gmail.com>
2024-03-18 12:57:47 +0545

    update

--- src/Components/CakeItem.jsx
+++ src/Components/CakeItem.jsx
@@ -5,7 +5,7 @@ const CakeItem = ({ cake, onAddToCart }) => {
     <div className="border p-4 mb-4 rounded-md shadow-md">
       <h2 className="text-xl font-semibold">{cake.name}</h2>
       <p className="text-gray-600">{cake.description}</p>
-      <img className="flex flex-wrap w-full h-full md:w-[420px] md:h-[420px] rounded-2xl pt-2" src={cake.image} alt={cake.name}/>
+      <img className="flex flex-wrap w-[420px] h-[420px] rounded-2xl pt-2" src={cake.image} alt={cake.name}/>
       <p className="text-gray-800 font-semibold mt-2">Rs.{cake.price}</p>
       <button
         className="mt-2 bg-[#F4BBC9] hover:bg-red-500 text-white px-4 py-2 rounded-md"
