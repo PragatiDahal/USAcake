85f1ce6916b839746042892d2781d5b8d7d1403c
PragatiDahal <pragatidahal2058@gmail.com>
2024-03-19 16:46:27 +0545

    update

--- src/Components/Trendingcake.jsx
+++ src/Components/Trendingcake.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import AOS from "aos";
 import "aos/dist/aos.css";
 import { useEffect } from "react";
-import {Link} from "react-router-dom"
+
 
 const Trendingcake = () => {
   useEffect(() => {
